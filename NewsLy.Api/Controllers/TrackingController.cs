b9578ffbcd3a4a1e3e7172dc0b4074f7ced7ea21
Philipp Moser <info@philipp-moser.de>
2021-10-24 11:29:39 +0200

    Add TrackingController to implement Redirection of TrackingUrls

---
+++ NewsLy.Api/Controllers/TrackingController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewsLy.Api.Repositories.Interfaces;
+
+namespace NewsLy.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TrackingController : ControllerBase
+    {
+        private readonly ILogger<TrackingController> _logger;
+        private readonly ITrackingUrlRepository _trackingUrlRepository;
+
+        public TrackingController(
+            ILogger<TrackingController> logger,
+            ITrackingUrlRepository trackingUrlRepository
+        )
+        {
+            _logger = logger;
+            _trackingUrlRepository = trackingUrlRepository;
+        }
+
+        [HttpGet]
+        public void Get([FromQuery] string t)
+        {
+            if(string.IsNullOrEmpty(t))
+            {
+                return;
+            }
+
+            var trackingUrl = _trackingUrlRepository.FindByTrackingId(t);
+
+            if(trackingUrl == null)
+            {
+                return;
+            }
+
+            Response.Redirect(trackingUrl.TargetUrl);
+
+            return;
+        }
+    }
+}
